1d16285b07e28a232f783ee46306a2ad19633ca4
sifatekeli <sifatkl62@gmail.com>
2016-10-11 15:08:14 +0200

    Ajout test histoire 1

--- src/Engine.js
+++ src/Engine.js
@@ -2,7 +2,12 @@
 
 var Engine = function () {
 
-// private attributes and methods
+    var board = new Array(new Array(6), new Array(6), new Array(6), new Array(6), new Array(6), new Array(6));
+    var colors = new Array("black", "blue", "green", "red", "white", "yellow");
+    var color_counter = new Array(6).fill(0);
 
-// public methods
+ 
+
+
 };
+
--- test/test.js
+++ test/test.js
@@ -1,6 +1,19 @@
 'use strict';
 
 var PalettoTestCase = TestCase("PalettoTestCase");
+var e = new Engine();
+var board = e.getBoard();
+var colors = e.getColors();
+var color_counter = e.getColorCounter();
 
 PalettoTestCase.prototype.testStory1 = function () {
+
+    e.init();
+
+    for(var line = 0; line < board.length; line++){
+        console.log(board[line]);
+    }
+
+
+    assertTrue(true);
 };
