d4734d38e5c0fb9a065e35dbe0b689e7b5868c59
sifatekeli <sifatkl62@gmail.com>
2016-10-11 15:32:20 +0200

    Envoi du fichier test oublié

--- test/test.js
+++ test/test.js
@@ -7,13 +7,15 @@ var colors = e.getColors();
 var color_counter = e.getColorCounter();
 
 PalettoTestCase.prototype.testStory1 = function () {
-
     e.init();
-
     for(var line = 0; line < board.length; line++){
         console.log(board[line]);
     }
-
-
     assertTrue(true);
+};
+
+PalettoTestCase.prototype.testStory2 = function () {
+    p1.takePiece(0,5);
+
+    assertTrue(board[0][5] == "yellow" && p1.yellow == 1);
 };
