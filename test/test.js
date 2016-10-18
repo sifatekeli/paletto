1fe1003ee4b0e5f847a900619193e4b4de68ea29
sifatekeli <sifatkl62@gmail.com>
2016-10-18 09:16:14 +0200

    Ajout du test 4

--- test/test.js
+++ test/test.js
@@ -9,20 +9,27 @@ var p1 = e.getP1();
 
 PalettoTestCase.prototype.testStory1 = function () {
     e.init();
-    for(var line = 0; line < board.length; line++){
-        console.log(board[line]);
-    }
+
     assertTrue(true);
 };
 
 PalettoTestCase.prototype.testStory2 = function () {
      e.takePiece("A6");
+    for(var line = 0; line < board.length; line++){
+        console.log(board[line]);
+    }
      assertTrue(board[5][0] == null && p1.yellow == 1);
 };
 
 PalettoTestCase.prototype.testStory3 = function () {
     assertTrue(e.nbPiece() == 35 && p1.yellow == 1 && board[5][0] == null);
-
 };
+
+PalettoTestCase.prototype.testStory4 = function () {
+    var current_player = e.currentPlayer();
+    e.takePiece("A1");
+    assertTrue(current_player == 2 && e.nbPiece() == 33 && p1.yellow == 1 && board[5][0] == null);
+};
+
 
 
