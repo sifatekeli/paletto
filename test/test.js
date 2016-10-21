6468fd7886aa7cf1eac44e2dd014f30a6f933010
sifatekeli <sifatkl62@gmail.com>
2016-10-21 21:28:44 +0200

    Ajout du test 6

--- test/test.js
+++ test/test.js
@@ -33,34 +33,59 @@ PalettoTestCase.prototype.testStory4 = function () {
 };
 
 PalettoTestCase.prototype.testStory5 = function () {
-    var current_player = e.currentPlayer();
-    board[0][0] = null;
-    board[0][1] = null;
-    board[0][2] = null;
-    board[1][0] = null;
-    board[1][1] = null;
-    board[1][2] = null;
-    board[1][5] = null;
-    board[2][0] = null;
-    board[2][1] = null;
-    board[2][5] = null;
-    board[3][3] = null;
-    board[3][4] = null;
-    board[3][5] = null;
-    board[4][0] = null;
-    board[4][3] = null;
-    board[4][4] = null;
-    board[4][5] = null;
-    board[5][0] = null;
-    board[5][1] = null;
-    board[5][3] = null;
-    board[5][4] = null;
-    board[5][5] = null;
+    var e1 = new Engine();
+    var board1 = e1.getBoard();
+    e1.init();
 
-    e.takePiece("D1");
+    board1[0][0] = null;
+    board1[0][1] = null;
+    board1[0][2] = null;
+    board1[1][0] = null;
+    board1[1][1] = null;
+    board1[1][2] = null;
+    board1[1][5] = null;
+    board1[2][0] = null;
+    board1[2][1] = null;
+    board1[2][5] = null;
+    board1[3][3] = null;
+    board1[3][4] = null;
+    board1[3][5] = null;
+    board1[4][0] = null;
+    board1[4][3] = null;
+    board1[4][4] = null;
+    board1[4][5] = null;
+    board1[5][0] = null;
+    board1[5][1] = null;
+    board1[5][3] = null;
+    board1[5][4] = null;
+    board1[5][5] = null;
 
-    assertTrue(board[0][3] == null);
-    assertTrue(board[2][2] == "blue");
+    e1.takePiece("D1");
+
+    assertTrue(board1[0][3] == null);
+    assertTrue(board1[2][2] == "blue");
+
+};
+
+
+PalettoTestCase.prototype.testStory6 = function () {
+    var e2 = new Engine();
+    var board2 = e2.getBoard();
+    e2.init();
+
+    e2.takePiece("A1");
+    e2.takePiece("B1");
+    e2.takePiece("A2");
+    e2.takePiece("A3");
+    e2.takePiece("A5");
+    e2.takePiece("E1");
+    e2.takePiece("C3");
+    e2.takePiece("B3");
+    e2.takePiece("B4");
+
+    var p1 = e2.getPlayer(0);
+
+    assertTrue(e.winByColor("black"));
 
 };
 
